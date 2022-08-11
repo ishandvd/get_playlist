c8f460fb148dbf9f4a910171d8b5251c14663e7d
TheChoconator <id360@cam.ac.uk>
2022-08-11 13:48:13 +0100

    Now can view artists albums

--- script.js
+++ script.js
@@ -8,32 +8,31 @@ btn.addEventListener('click', () => {
     input.focus()
 })
 
-
+// Now, we have the auth working in this... We want to be able to display a 
+// picture of the album art.
 
 const base = 'https://api.spotify.com/'
 console.log(base)
 const CLIENT_ID = 'b189b96c428d420988bc622dbe88ce57';
 const CLIENT_SECRET = 'd8282bd6f657411c827c4f7de0bf5c26';
 
+var SPOTIFY_AUTH_TOKEN = ''
 
-async function getAuthToken() {
 
-    const result = await fetch(`https://accounts.spotify.com/api/token`, {
+fetch(`https://accounts.spotify.com/api/token`, {
         method: 'POST',
         headers: { 'content-type': 'application/x-www-form-urlencoded',
                     'Authorization': 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET), 
                      },
         body : 'grant_type=client_credentials'
-      });
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log("Testing for ponushonu too poo: " + data.access_token)
+        SPOTIFY_AUTH_TOKEN = data.access_token
+        
+    });
 
-    const data = await result.json();
-
-    console.log("Auth Token: " + data.access_token)
-
-    return data.access_token;
-
-
-}
 
 // var authToken = '';
 const authToken = new Promise(() => {getAuthToken()});
@@ -54,41 +53,63 @@ var fetchTracks = function (albumId, callback) {
 const albums = document.getElementById('albums');
 
 
-var searchAlbums = async function (query) {
-
+async function searchAlbums(query) {
     var output = [];
 
-    if(authToken === ''){
-        const empty = await new Promise(r => setTimeout(r, 1000));
-    }
-
+    // fetch(`https://api.spotify.com/v1/search`, {
+    //     method: 'GET',
+    //     headers: { 'Authorization': 'Bearer ' + SPOTIFY_AUTH_TOKEN},
+    //     data : {
+    //         q: query,
+    //         type: 'album'
+    //     }
+    //   })
+    //   .then(res => console.log(res))
+    //   .then(data => {
+    //     console.log("Toopie 1 2 3: " + data)
+    //     }
+        // );
+    
     $.ajax({
         url: 'https://api.spotify.com/v1/search',
         type: 'GET',
         beforeSend: async function (xhr) {
             // const forAuth = await authToken;
-            xhr.setRequestHeader('Authorization', 'Bearer ' + 'BQBs1aG_D-05OyLEXZ0QIoYsvPOo206bZlukrfx-RsNrzW4orobuyEAUQJDUHzNVgPBX6W04A1WWFdKzeEUWTbjo0w2RrivxjXAZCL8iylYXVv_Lat0');
+            xhr.setRequestHeader('Authorization', 'Bearer ' + SPOTIFY_AUTH_TOKEN);
         },
         data: {
             q: query,
             type: 'album',
+            limit: '10'
         },
         success: function (data) {
             output = data.albums.items;
-            console.log(output)
-
+            console.log("Search Albums output: " + output)
             output.forEach((obj) => {
 
-                // console.log(obj);
-                var li = document.createElement("li");
-                li.appendChild(document.createTextNode(obj.name));
-                albums.appendChild(li);
-                console.log('added');
-
+                console.log(obj);
+        
+                // var li = document.createElement("li");
+                // li.appendChild(document.createTextNode(obj.name));
+                // albums.appendChild(li);
+                // console.log('added');
+                var album = document.createElement("div")
+                album.classList.add('album')
+                album.innerHTML = 
+                `
+                    <div class="wrapper">
+                        <img src="${obj.images[1].url}" class="albumArt">
+                    </div>
+                    <div class="albumInfo">
+                        <h1 class="albumTitle">${obj.name}</h1>
+                        <p>Released: ${obj.release_date}<br>
+                            Track Count:  ${obj.total_tracks}</p>
+                    </div>
+                `
+                albums.appendChild(album)
+                
             })
-
-
-
+            return output;
         },
         error: function (data) {
             console.log("Error: ")
@@ -98,26 +119,28 @@ var searchAlbums = async function (query) {
         }
     });
 
-    console.log(output)
-
-    return output;
 };
-
-// searchAlbums('led zeppelin');
 
 var searchbar = document.getElementById("input");
 
-input.addEventListener("keyup", function(event) {
+
+// Upon entering album name into search bar, display the
+// albums.
+input.addEventListener("keyup", async function(event) {
     // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
       // Cancel the default action, if needed
       event.preventDefault();
       // Trigger the button element with a click
 
+      // remove current albums
       while( albums.firstChild ){
         albums.removeChild( albums.firstChild );
       }
-      searchAlbums(searchbar.value);
+      const albumList = await searchAlbums(searchbar.value);
+      console.log("Album List: " + albumList)
+
+
     }
   });
 
